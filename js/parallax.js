8fde35ac15baf27b1e95a952e2502c628fa12ff2
annapancioli <annapancioli@gmail.com>
2018-12-06 07:16:19 -0500

    edited files, created about

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var parallax= document.querySelector(".parallax");
+var parallax2= document.querySelector(".parallax2");
 
 window.addEventListener("scroll", function() {
 
@@ -12,5 +13,18 @@ window.addEventListener("scroll", function() {
  
     } else {
      parallax.style.backgroundPositionY=  "0";
+    }
+    });
+
+  window.addEventListener("scroll", function() {
+
+    var scrolledHeight= window.pageYOffset,
+                  limit= parallax2.offsetTop+ parallax2.offsetHeight;
+                   
+  if(scrolledHeight > parallax2.offsetTop && scrolledHeight <= limit) {
+    parallax2.style.backgroundPositionY=  (scrolledHeight - parallax2.offsetTop) /1.5+ "px";
+    
+    } else {
+    parallax2.style.backgroundPositionY=  "0";
     }
     });
